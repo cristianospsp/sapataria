247bf53c3a692e914f7429e862247b8fab17094f
Cristiano de Oliveira <cristianospsp@gmail.com>
2015-10-20 20:02:57 -0200

    criado modelo Pedido, para teste, falta colocar todos os atributos;.....

---
+++ src/main/java/br/com/dwd/sapataria/model/Pedido.java
@@ -0,0 +1,36 @@
+package br.com.dwd.sapataria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Cristiano on 19/10/15.
+ */
+@Entity
+public class Pedido implements Serializable {
+
+	@Id
+	private Long id;
+	@OneToMany
+	private List<Produto> produtos = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+}
