f77e834e4b42e59fee8ce1ffadc7da43e6487ec0
Elizeu S. Bandeira <elizeusbandeira@gmail.com>
2015-10-27 13:42:36 -0200

    Mudancas na funcionalidade funcionario
    Incluido UsuarioController
    Incluido UsuarioEntityManager
    Incluido  UsuaeioTask
    Feito alteracao na tela

---
+++ src/main/java/br/com/dwd/sapataria/task/UsuarioTask.java
@@ -0,0 +1,46 @@
+package br.com.dwd.sapataria.task;
+
+import br.com.dwd.sapataria.dao.UsuarioEntityManager;
+import br.com.dwd.sapataria.dao.Repository;
+import br.com.dwd.sapataria.model.Usuario;
+
+import java.io.Serializable;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.transaction.Transactional;
+
+@Named
+@Resource
+public class UsuarioTask implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Repository<Usuario> repository;
+
+	private EntityManager entitymanager = UsuarioEntityManager.getEntityManager();
+	
+	@Transactional
+	public Usuario add(Usuario usuario) {
+		EntityTransaction transaction = entitymanager.getTransaction();
+		transaction.begin();
+		entitymanager.persist(usuario);
+		transaction.commit();
+		repository.add(usuario);
+		return usuario;
+	}
+
+	public Repository<Usuario> getRepository() {
+		return repository;
+	}
+
+	public void setRepository(Repository<Usuario> repository) {
+		this.repository = repository;
+	}
+}
