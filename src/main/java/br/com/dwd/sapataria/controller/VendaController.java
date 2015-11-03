0b985e5a50083b2f9d43ccd31778293ade1a53f7
Cristiano de Oliveira <cristianospsp@gmail.com>
2015-11-03 20:28:16 -0200

    inicio da implementacao da parte de vendas

--- src/main/java/br/com/dwd/sapataria/controller/VendaController.java
+++ src/main/java/br/com/dwd/sapataria/controller/VendaController.java
@@ -1,6 +1,8 @@
 package br.com.dwd.sapataria.controller;
 
 
+import br.com.dwd.sapataria.model.Pedido;
+import br.com.dwd.sapataria.model.Produto;
 import br.com.dwd.sapataria.model.Venda;
 import br.com.dwd.sapataria.task.VendaTask;
 import org.omnifaces.util.Faces;
@@ -15,8 +17,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -27,14 +28,46 @@ import java.util.List;
 public class VendaController implements Serializable {
 
 	private List<Venda> vendas = new ArrayList<>();
-
+	private List<Pedido> pedidos = new ArrayList<>();
 	@Inject
 	private VendaTask task;
+	private Pedido pedidoSelecionado;
 
 
 	@PostConstruct
 	public void init() {
 		refresh();
+		pedidos.addAll(geraPedido());
+	}
+
+	private List<Pedido> geraPedido() {
+		List<Pedido> pedidos = new ArrayList<>();
+
+		Produto adidas = new Produto("Tenis", "Adidas", "Branco", 340.00);
+		Produto nike = new Produto("Tenis", "Nike", "Azul", 350.00);
+		Produto mizuno = new Produto("Tenis", "Mizuno", "Preto", 450.00);
+
+		Produto chinelo = new Produto("Chinelo", "Havaianas", "Azul", 40.00);
+		Produto sapato = new Produto("Sapato", "Nosferato", "Preto", 150.00);
+		Produto sandalia = new Produto("Sandália", "Azaléia", "Brancao", 50.00);
+
+		Pedido pedido1 = new Pedido();
+		pedido1.setNumero(123456l);
+		pedido1.setVendedor("Kebra Barraco");
+		pedido1.adicionaProduto(adidas, 1);
+		pedido1.adicionaProduto(nike, 1);
+		pedido1.adicionaProduto(mizuno, 1);
+
+		Pedido pedido2 = new Pedido();
+		pedido2.setNumero(44444l);
+		pedido2.setVendedor("Kebra Apartamento");
+		pedido2.adicionaProduto(chinelo, 1);
+		pedido2.adicionaProduto(sapato, 1);
+		pedido2.adicionaProduto(sandalia, 1);
+
+		pedidos.addAll(Arrays.asList(pedido1, pedido2));
+
+		return pedidos;
 	}
 
 	public void refresh() {
@@ -46,12 +79,27 @@ public class VendaController implements Serializable {
 	}
 
 
-	public void vender() throws IOException {
+	public String vender() {
 		FacesContext context = FacesContext.getCurrentInstance();
-		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Vendeu ! ! !", "Vendeu ! ! !"));
+		context.addMessage("Teste", new FacesMessage(FacesMessage.SEVERITY_INFO, "Vendeu ! ! !", "Vendeu ! ! !"));
 		//FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
-		Faces.redirect("/sapataria/restrito/venda/lista.xhtml");
+		//Faces.redirect("/sapataria/restrito/venda/lista.xhtml");
+		return "lista?faces-redirect=true";
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
 
+	public void select(Pedido pedido) {
+		this.pedidoSelecionado = pedido;
+	}
+
+	public Pedido getPedidoSelecionado() {
+		return pedidoSelecionado;
+	}
+
+	public void limparSelecao() {
+		this.pedidoSelecionado = null;
+	}
 }
