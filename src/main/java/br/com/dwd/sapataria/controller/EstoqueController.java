bd9c6505a840fe22fb7ac643ba21805ceeec82e1
Cristiano de Oliveira <cristianospsp@gmail.com>
2015-12-08 23:37:26 -0200

    criado Controller para verificar qtd de produtos no estoque

---
+++ src/main/java/br/com/dwd/sapataria/controller/EstoqueController.java
@@ -0,0 +1,33 @@
+package br.com.dwd.sapataria.controller;
+
+import br.com.dwd.sapataria.model.Produto;
+import br.com.dwd.sapataria.task.ProdutoTask;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Cristiano on 08/12/15.
+ */
+@Named
+@ViewScoped
+public class EstoqueController implements Serializable {
+
+	private List<Produto> produtos = new ArrayList<>();
+
+	@Inject
+	private ProdutoTask task;
+
+	public void atualizar() {
+		produtos = task.findQtdMenorQueDez();
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+}
